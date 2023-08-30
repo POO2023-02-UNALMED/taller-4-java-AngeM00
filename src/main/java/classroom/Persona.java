c8f5372977e0c05ba8c60d72e2dd5aee2fad2cda
Angelika Moya <angelmb023@gmail.com>
2023-08-30 12:58:44 -0500

    update

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -1,14 +1,16 @@
 package classroom;
 
 public class Persona {
-
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
+        //cedula = 3;
+    }
+
+    public Persona(){
     }
 
     public Persona(long cedula, String nombre) {
@@ -31,6 +33,7 @@ public class Persona {
 
     public Persona(String nombre) {
         this.nombre = "";
+        this.cedula = 1;
         totalPersonas++;
     }
     
